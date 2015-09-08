775a49065c58d1dd5a2565f68081ea0adb25597c
Alexandra <sakujo8744@mail.ru>
2015-09-08 11:45:31 +0300

    html and css files created.

--- src/main/java/cards/Main.java
+++ src/main/java/cards/Main.java
@@ -1,7 +1,15 @@
 package cards;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by alexandra on 06.09.15.
  */
+
+
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
