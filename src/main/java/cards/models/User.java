e1f77b6e3a58efe5566cde45693c2788c295efb3
Alexandra <sakujo8744@mail.ru>
2015-09-09 20:28:59 +0300

    back: removed password from User

--- src/main/java/cards/models/User.java
+++ src/main/java/cards/models/User.java
@@ -9,14 +9,12 @@ public class User {
 
     private final String login;
     private final String email;
-    private final String passwd;
     private final boolean gender;
     private final Date birthDate;
 
-    public User(String login, String email, String passwd, boolean gender, Date birthDate) {
+    public User(String login, String email, boolean gender, Date birthDate) {
         this.login = login;
         this.email = email;
-        this.passwd = passwd;
         this.gender = gender;
         this.birthDate = birthDate;
     }
@@ -27,10 +25,6 @@ public class User {
 
     public String getEmail() {
         return email;
-    }
-
-    public String getPasswd() {
-        return passwd;
     }
 
     public boolean isGender() {
