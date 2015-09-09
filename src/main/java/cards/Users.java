a0e20507f2d0b2ef0e20a439355e7a8905d28803
Alexandra <sakujo8744@mail.ru>
2015-09-09 20:11:17 +0300

    back: created Users access class

---
+++ src/main/java/cards/Users.java
@@ -0,0 +1,14 @@
+package cards;
+
+import cards.models.User;
+
+/**
+ * Created by alexandra on 9/9/15.
+ */
+public class Users {
+
+    public static User getUser(String login){
+        return null;
+    }
+
+}
