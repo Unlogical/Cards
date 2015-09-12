97850cf0501f340cf0e8a0fd2ce42b756a116e9d
Alexandra <sakujo8744@mail.ru>
2015-09-12 19:06:57 +0300

    back: get data from database

--- src/main/java/cards/Users.java
+++ src/main/java/cards/Users.java
@@ -2,6 +2,10 @@ package cards;
 
 import cards.models.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 /**
@@ -9,11 +13,39 @@ import java.util.Date;
  */
 public class Users {
 
+    private static Connection connection = connect("jdbc:postgresql://192.168.56.101/resu_db", "resu", "resu");
+
+    private static Connection connect(String connectionString, String username, String password){
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(connectionString, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
     public static User getUser(String login){
+
+        try {
+            ResultSet resultSet = connection.createStatement().executeQuery("select * from users where login='" + login + "'");
+            while(resultSet.next()){
+                String email = resultSet.getString("email");
+                System.out.println(email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         return new User(login, login + "@lol.com", true, new Date(System.currentTimeMillis()));
     }
 
     public static boolean addUser(String login, String passwd, String email, boolean gender, Date birthDate) {
         return true;
     }
+
 }
