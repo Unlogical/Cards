6b0fd222467d837d590bf763ca6d43a2599385a4
Alexandra <sakujo8744@mail.ru>
2015-09-24 18:53:27 +0300

    back: SessionManager created

---
+++ src/main/java/cards/SessionManager.java
@@ -0,0 +1,12 @@
+package cards;
+import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
+
+/**
+ * Created by alexandra on 9/24/15.
+ */
+public class SessionManager {
+    public  static String createSession(String login){
+        String sessionID = sha256Hex(login + System.currentTimeMillis());
+
+    }
+}
