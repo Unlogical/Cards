834a8893be201a14a1c0ae45726b8b62f5ccd329
Alexandra <sakujo8744@mail.ru>
2015-09-27 15:48:20 +0300

    try pool

--- src/main/java/cards/SessionManager.java
+++ src/main/java/cards/SessionManager.java
@@ -7,6 +7,6 @@ import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
 public class SessionManager {
     public  static String createSession(String login){
         String sessionID = sha256Hex(login + System.currentTimeMillis());
-
+        return null;
     }
 }
--- src/main/java/cards/Users.java
+++ src/main/java/cards/Users.java
@@ -1,6 +1,8 @@
 package cards;
 
 import cards.models.User;
+import org.postgresql.jdbc2.optional.ConnectionPool;
+
 import java.sql.*;
 import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
 
@@ -9,25 +11,28 @@ import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
  */
 public class Users {
 
-    private static Connection connection = connect("jdbc:postgresql://192.168.56.101/resu_db", "resu", "resu");
+    private static ConnectionPool connectionPool = connect("jdbc:postgresql://192.168.56.101/resu_db", "resu", "resu");
 
-    private static Connection connect(String connectionString, String username, String password){
-        Connection connection = null;
+    private static ConnectionPool connect(String connectionString, String username, String password){
+        ConnectionPool pool = null;
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(connectionString, username, password);
+            pool = new ConnectionPool();
+            pool.setUrl(connectionString);
+            pool.setUser(username);
+            pool.setPassword(password);
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return connection;
+        return pool;
     }
 
     public static User getUser(String login){
 
         try {
-            ResultSet resultSet = connection.createStatement().executeQuery("select * from users where login='" + login + "'"); // TODO: use prepared statements  ( '; drop table lol;-- )
+            ResultSet resultSet = connectionPool.getConnection().createStatement().executeQuery("select * from users where login='" + login + "'"); // TODO: use prepared statements  ( '; drop table lol;-- )
             while(resultSet.next()){
                 String email = resultSet.getString("email");
                 Boolean gender = resultSet.getBoolean("gender");
@@ -43,7 +48,7 @@ public class Users {
     public static boolean addUser(String login, String passwd, String email, boolean gender) {
         //TODO check this data
         try {
-            PreparedStatement insertUser = connection.prepareStatement("insert into users(login, passwd, email, gender) values (?,?,?,?,?)");
+            PreparedStatement insertUser = connectionPool.getConnection().prepareStatement("insert into users(login, passwd, email, gender) values (?,?,?,?,?)");
             insertUser.setString(1, login);
             insertUser.setString(2, sha256Hex(passwd));
             insertUser.setString(3, email);
@@ -58,7 +63,7 @@ public class Users {
     public static boolean checkPassword(String login, String password){
         String passwordHash = sha256Hex(password);
         try {
-            PreparedStatement checkPasswd = connection.prepareStatement("select passwd from users where login=?");
+            PreparedStatement checkPasswd = connectionPool.getConnection().prepareStatement("select passwd from users where login=?");
             checkPasswd.setString(1, login);
             ResultSet resultSet = checkPasswd.executeQuery();
 
