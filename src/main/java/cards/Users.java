3a7a2bc6da4da7f51378e4dde191d24aaa80bc38
Alexandra <sakujo8744@mail.ru>
2015-09-27 17:04:47 +0300

    back: connections in Users now with connection provider

--- src/main/java/cards/Users.java
+++ src/main/java/cards/Users.java
@@ -1,9 +1,12 @@
 package cards;
 
 import cards.models.User;
-import org.postgresql.jdbc2.optional.ConnectionPool;
 
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static cards.ConnectionProvider.getConnectionProvider;
 import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
 
 /**
@@ -14,29 +17,29 @@ public class Users {
     public static User getUser(String login){
 
         try {
-            ResultSet resultSet = connectionPool.getConnection().createStatement().executeQuery("select * from users where login='" + login + "'"); // TODO: use prepared statements  ( '; drop table lol;-- )
-            while(resultSet.next()){
+            ResultSet resultSet = getConnectionProvider().getConnection().createStatement().executeQuery("select * from users where login='" + login + "'"); // TODO: use prepared statements  ( '; drop table lol;-- )
+            if(resultSet.next()){
                 String email = resultSet.getString("email");
                 Boolean gender = resultSet.getBoolean("gender");
                 return new User(login, email, gender);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-         return null;
+        return null;
     }
 
     public static boolean addUser(String login, String passwd, String email, boolean gender) {
         //TODO check this data
         try {
-            PreparedStatement insertUser = connectionPool.getConnection().prepareStatement("insert into users(login, passwd, email, gender) values (?,?,?,?,?)");
+            PreparedStatement insertUser = getConnectionProvider().getConnection().prepareStatement("insert into users(login, passwd, email, gender) values (?,?,?,?,?)");
             insertUser.setString(1, login);
             insertUser.setString(2, sha256Hex(passwd));
             insertUser.setString(3, email);
             insertUser.setBoolean(4, gender);
             return insertUser.execute();
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return false;
@@ -45,7 +48,7 @@ public class Users {
     public static boolean checkPassword(String login, String password){
         String passwordHash = sha256Hex(password);
         try {
-            PreparedStatement checkPasswd = connectionPool.getConnection().prepareStatement("select passwd from users where login=?");
+            PreparedStatement checkPasswd = getConnectionProvider().getConnection().prepareStatement("select passwd from users where login=?");
             checkPasswd.setString(1, login);
             ResultSet resultSet = checkPasswd.executeQuery();
 
@@ -54,7 +57,7 @@ public class Users {
                 String basePasswordHash = resultSet.getString(1);
                 return basePasswordHash.equals(passwordHash);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         System.out.println("Checking failed or bad password");
