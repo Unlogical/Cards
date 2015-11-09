ced4ba83b02d0d5e77b6a088e4d747d7d7b6a3ef
Alexandra <sakujo8744@mail.ru>
2015-11-09 19:10:38 +0300

    front:creauserpage^ one set js delete(display none)

--- front-end/javascript/userpage.js
+++ front-end/javascript/userpage.js
@@ -1,10 +1,6 @@
 function deleteset(id) {
   document.getElementById(id).style.display = "none"  	
 }
- function show() {
-  alert("rwrligjilk")    
-}
-
 function init() {
    document.getElementById("delete-set1").onclick = function () {
     deleteset("set1")
