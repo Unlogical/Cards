bde524abcbc52686c907a7fe04144753bc59910a
Alexandra <sakujo8744@mail.ru>
2015-11-09 19:09:42 +0300

    front:creauserpage^ one set js delete(displsy none)

--- front-end/javascript/userpage.js
+++ front-end/javascript/userpage.js
@@ -1,7 +1,13 @@
-
-
-init() {
-  document.getElementsByClass("fa-trash").onClick = showModalDialog("sure?")
+function deleteset(id) {
+  document.getElementById(id).style.display = "none"  	
+}
+ function show() {
+  alert("rwrligjilk")    
 }
 
+function init() {
+   document.getElementById("delete-set1").onclick = function () {
+    deleteset("set1")
+  }
+}
 window.addEventListener('load',init,false)
