3c5485a4d25db0e737c77dd8ebedfaa6711140a7
Alexandra <sakujo8744@mail.ru>
2015-11-15 23:19:19 +0300

    back: users is not singleton now

--- src/main/java/cards/Users.java
+++ src/main/java/cards/Users.java
@@ -5,11 +5,6 @@ import cards.models.User;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.*;
-import java.util.regex.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import static cards.ConnectionProvider.getConnectionProvider;
 import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
 
 /**
@@ -17,35 +12,35 @@ import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
  */
 public class Users {
 
-    public static User getUser(String login) {
+    private final ConnectionProvider connectionProvider;
+
+    public Users(ConnectionProvider connectionProvider) {
+        this.connectionProvider = connectionProvider;
+    }
+
+    public User getUser(String login) {
 
         try {
-            PreparedStatement getuser = getConnectionProvider().getConnection().prepareStatement("select * from users where login =? ");
+            PreparedStatement getuser = connectionProvider.getConnection().prepareStatement("select * from users where login =? ");
             getuser.setString(1,login);
+            ResultSet resultSet = getuser.executeQuery();
+            if(resultSet.next()){
+                String email = resultSet.getString("email");
+                Boolean gender = resultSet.getBoolean("gender");
+                return new User(login, email, gender);
+            }
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-
-//        try {
-//            ResultSet resultSet = connection.createStatement().executeQuery("select * from users where login='" + login + "'"); // TODO: use prepared statements  ( '; drop table lol;-- )
-//            while(resultSet.next()){
-//                String email = resultSet.getString("email");
-//                Boolean gender = resultSet.getBoolean("gender");
-//                return new User(login, email, gender);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-
          return null;
     }
 
-    public static boolean addUser(String login, String passwd, String email, boolean gender) {
+    public boolean addUser(String login, String passwd, String email, boolean gender) {
         //TODO check this data
         String[] data = new String[3];
-        data[1] = login;
-        data[2] = passwd;
-        data[3] = email;
+        data[0] = login;
+        data[1] = passwd;
+        data[2] = email;
         for(String str : data){
             if(str == null || str == "" ) return false;
         }
@@ -58,25 +53,25 @@ public class Users {
 
 
         try {
-            PreparedStatement insertUser = getConnectionProvider().getConnection().prepareStatement("insert into users(login, passwd, email, gender) values (?,?,?,?,?)");
+            PreparedStatement insertUser = connectionProvider.getConnection().prepareStatement("insert into users(login, passwd, email, gender) values (?,?,?,?)");
             insertUser.setString(1, login);
             insertUser.setString(2, sha256Hex(passwd));
             insertUser.setString(3, email);
             insertUser.setBoolean(4, gender);
-            return insertUser.execute();
+            insertUser.execute();
+            return true;
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return false;
     }
 
-    public static boolean checkPassword(String login, String password){
+    public boolean checkPassword(String login, String password){
         String passwordHash = sha256Hex(password);
         try {
-            PreparedStatement checkPasswd = getConnectionProvider().getConnection().prepareStatement("select passwd from users where login=?");
+            PreparedStatement checkPasswd = connectionProvider.getConnection().prepareStatement("select passwd from users where login=?");
             checkPasswd.setString(1, login);
             ResultSet resultSet = checkPasswd.executeQuery();
-
             if(resultSet.next()) {
                 System.out.println("Checking password");
                 String basePasswordHash = resultSet.getString(1);
