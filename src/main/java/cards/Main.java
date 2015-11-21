0e1a98ffc2c0dbc25245108c5ded995a005c375d
Alexandra <sakujo8744@mail.ru>
2015-11-21 17:17:53 +0300

    fix show

--- src/main/java/cards/Main.java
+++ src/main/java/cards/Main.java
@@ -1,6 +1,7 @@
 package cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 /**
